096da34fb3cf28e509cb901a79597162767db99c
junipera <juniper.alcorn@gmail.com>
2019-05-03 15:13:16 -0400

    finish auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :authorize_request, except: :create
   # GET /users
   # GET /users.json
   def index
