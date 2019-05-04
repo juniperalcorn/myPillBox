bcdd42daa3ea805bde5dba78ede23cc6b00ab4bd
junipera <juniper.alcorn@gmail.com>
2019-05-04 19:54:43 -0400

    pills controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   resources :users
+  resources :pills
+  # resources :doses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
