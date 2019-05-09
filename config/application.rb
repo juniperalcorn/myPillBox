8381f8ebdefe1a3c6ddaa28122a7bd5e6bd59996
junipera <juniper.alcorn@gmail.com>
2019-05-09 12:22:06 -0400

    config for api

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module PillOrganizer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    ##added may 9
+    config.api_only = true;
   end
 end
 
