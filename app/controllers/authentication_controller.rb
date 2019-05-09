81756fcfc7ef1e36f4323598cc4435d8d074d400
junipera <juniper.alcorn@gmail.com>
2019-05-09 13:30:20 -0400

    change user.find_by on app control

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,6 +4,8 @@ class AuthenticationController < ApplicationController
     
     def login
         @user = User.find_by_username(params[:username])
+        @user = User.find_by(username: params[:username])
+
         
         if @user.authenticate(params[:password])
             token = JsonWebToken.encode(user_id: @user.id, username: @user.username)
