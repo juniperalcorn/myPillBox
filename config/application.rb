32636286a6528c6294f404826f7ffd105d708372
junipera <juniper.alcorn@gmail.com>
2019-05-09 10:49:55 -0400

    modify cors file

--- config/application.rb
+++ config/application.rb
@@ -15,14 +15,14 @@ module PillOrganizer
 
     config.autoload_paths << Rails.root.join('lib')
 
-    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', 
-          headers: :any, 
-          methods: [:get, :post, :put, :patch, :options, :head]
-      end
-    end
+    # config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', 
+    #       headers: :any, 
+    #       methods: [:get, :post, :put, :patch, :options, :head]
+    #   end
+    # end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
